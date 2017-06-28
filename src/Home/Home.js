735b458a6648a3a62d33b14acb4ae63edbbe15f9
Bartosz Galek <bartosz.galek@allegrogroup.com>
2017-06-28 15:30:59 +0200

    home screen updated

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,42 @@
 // @flow
 import React, {Component} from 'react';
 import './Home.css';
+import {Card, CardHeader, CardMedia, CardTitle} from "material-ui";
 
 export default class Home extends Component {
-
-    authenticatedContent() {
-        return <h4>You are logged in!</h4>
-    }
-
-    anonymousContent(login) {
-        return <h4>You are not logged in! Please{' '} <a style={{cursor: 'pointer'}} onClick={login.bind(this)}>Log In</a> to continue.</h4>
-    }
-
     render() {
-        return this.props.auth.isAuthenticated() ? this.authenticatedContent() : this.anonymousContent(this.props.auth.login);
+        return (
+            <div>
+                <Card>
+                    <CardMedia overlay={<CardTitle title="Tytuł wystąpienia"
+                                                   subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin."/>}>
+                        <img src="https://www.pixelstalk.net/wp-content/uploads/2016/04/Google-Wallpaper-Good-Full-HD.png" alt=""/>
+                    </CardMedia>
+                    <CardHeader title="Speaker Speakerowy" subtitle="8 minut do końca"
+                                avatar="http://www.material-ui.com/images/jsa-128.jpg"/>
+                </Card>
+                <h2>Kolejne wystąpienia</h2>
+                <Card style={{margin: '30px 0'}}>
+                    <CardTitle title="Title Title Title Title Title Title"/>
+                    <CardHeader title="Speaker Speakerowy" subtitle="8 minut do końca"
+                                avatar="http://www.material-ui.com/images/jsa-128.jpg"/>
+                </Card>
+                <Card style={{margin: '30px 0'}}>
+                    <CardTitle title="Title Title Title Title Title Title"/>
+                    <CardHeader title="Speaker Speakerowy" subtitle="8 minut do końca"
+                                avatar="http://www.material-ui.com/images/jsa-128.jpg"/>
+                </Card>
+                <Card style={{margin: '30px 0'}}>
+                    <CardTitle title="Title Title Title Title Title Title"/>
+                    <CardHeader title="Speaker Speakerowy" subtitle="8 minut do końca"
+                                avatar="http://www.material-ui.com/images/jsa-128.jpg"/>
+                </Card>
+                <Card style={{margin: '30px 0'}}>
+                    <CardTitle title="Title Title Title Title Title Title"/>
+                    <CardHeader title="Speaker Speakerowy" subtitle="8 minut do końca"
+                                avatar="http://www.material-ui.com/images/jsa-128.jpg"/>
+                </Card>
+            </div>
+        )
     }
 }
