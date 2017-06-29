580f755ea3efdd3f2cef3767cf6071de9d5bebc4
Bartosz Galek <bartosz.galek@allegrogroup.com>
2017-06-29 17:07:07 +0200

    animation added

--- src/Views/Schedule/ScheduleItem.js
+++ src/Views/Schedule/ScheduleItem.js
@@ -4,6 +4,7 @@ import {PropTypes} from 'prop-types';
 import {Avatar, Card, CardHeader, CardText, CardTitle} from "material-ui";
 import './ScheduleItem.css';
 import SocialPerson from 'material-ui/svg-icons/social/person';
+import {CSSTransitionGroup} from 'react-transition-group'
 
 export default class ScheduleItem extends Component {
 
@@ -19,11 +20,21 @@ export default class ScheduleItem extends Component {
 
     render() {
         return (
-            <Card className="schedule-card" onTouchTap={() => this.props.action(this.props.id)}>
-                <CardTitle title={this.props.title}/>
-                <CardText>{this.props.content}</CardText>
-                <CardHeader title={this.props.speaker} subtitle={this.props.hour} avatar={this.props.avatar || <Avatar icon={<SocialPerson/>}/>}/>
-            </Card>
+            <CSSTransitionGroup
+                key={this.props.id}
+                transitionName="schedule-card-animation"
+                transitionAppear={true}
+                transitionAppearTimeout={500}
+                transitionEnter={false}
+                transitionLeave={false}>
+                <Card className="schedule-card" onTouchTap={() => this.props.action(this.props.id)}>
+                    <CardTitle title={this.props.title}/>
+                    <CardText>{this.props.content}</CardText>
+                    <CardHeader title={this.props.speaker}
+                                subtitle={this.props.hour}
+                                avatar={this.props.avatar || <Avatar icon={<SocialPerson/>}/>}/>
+                </Card>
+            </CSSTransitionGroup>
         )
     }
 }
