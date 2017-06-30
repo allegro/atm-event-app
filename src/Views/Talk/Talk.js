de04ac28dae3b11ec785fe0e3c2bedb2777a5d77
Bartosz Galek <bartosz.galek@allegrogroup.com>
2017-06-30 17:13:13 +0200

    talk view added

--- src/Views/Talk/Talk.js
+++ src/Views/Talk/Talk.js
@@ -1,6 +1,9 @@
 // @flow
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
+import {Avatar, Paper} from "material-ui";
+import schedule from '../Schedule/schedule.json';
+import slugify from 'slugify';
 
 export default class Talk extends Component {
 
@@ -9,8 +12,15 @@ export default class Talk extends Component {
     };
 
     render() {
+        const item = schedule[0].agenda.find(it => slugify(it.title) === this.props.match.params.id);
         return (
-            <p>{this.props.match.params.id}</p>
+            <div style={{textAlign: 'center', margin: '0 auto', padding: '30px'}}>
+                <Paper style={{padding: '30'}} zDepth={1}>
+                    {item.photo ? <Avatar size={160} src={item.photo}/> : null}
+                    <h2>{item.speaker}</h2>
+                    <h3>{item.title}</h3>
+                </Paper>
+            </div>
         )
     }
 }
