8c24239bc70250f775c13898ccc204079eeb920d
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@allegro.pl>
2019-08-26 12:32:36 +0200

    Updates

---
+++ scanner/precache-manifest.04554d6203c4024a261cf674692a0207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3447820f77502a09588f794221897ac5",
+    "url": "/atm-event-app/scanner/index.html"
+  },
+  {
+    "revision": "7ab4d19ef57debe74a87",
+    "url": "/atm-event-app/scanner/static/css/main.aafb7e44.chunk.css"
+  },
+  {
+    "revision": "2ab151286e40297f5a00",
+    "url": "/atm-event-app/scanner/static/js/2.dd312e86.chunk.js"
+  },
+  {
+    "revision": "7ab4d19ef57debe74a87",
+    "url": "/atm-event-app/scanner/static/js/main.172d57ce.chunk.js"
+  },
+  {
+    "revision": "3e0af73a4b1e7a16ca14",
+    "url": "/atm-event-app/scanner/static/js/runtime~main.cc592acb.js"
+  },
+  {
+    "revision": "1798b9731a4f87231d41c8ccf92e14a7",
+    "url": "/atm-event-app/scanner/static/media/scanner.1798b973.svg"
+  }
+]);
